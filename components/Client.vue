070bd141cfc7952863a27a76e49fe9a3fdce5077
Vap0r1ze <superdash993@gmail.com>
2024-04-26 05:13:10 -0400

    test patched url

--- components/Client.vue
+++ components/Client.vue
@@ -1,10 +1,17 @@
 <<script setup lang="ts">
 const { data: session, status: sessionStatus } = useFetch('/api/join', { method: 'post', credentials: 'include' })
+
+function patchEmbedUrl(embed: string) {
+    const url = new URL(embed)
+    const [id1] = url.hostname.split('.')
+    const id2 = url.pathname.slice(1)
+    return window.location.origin + `/vm/${id1}/${id2}${url.search}`
+}
 </script>
 
 <template>
     <div class="relative overflow-clip h-screen" v-if="session">
-        <Hyperbeam :embed="session.embed" />
+        <Hyperbeam :embed="patchEmbedUrl(session.embed)" />
         <!-- <div
             v-for="[userId, pos] in Object.entries(cursors)"
             :key="userId"
