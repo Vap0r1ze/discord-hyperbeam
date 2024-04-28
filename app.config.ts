c30ae6e57e45979464a0f88b6fe2c4abb2c749ec
Vap0r1ze <superdash993@gmail.com>
2024-04-28 08:44:26 -0400

    icons ? ? ??

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
     nuxtIcon: {
         iconifyApiOptions: {
-            url: `https://${process.env.NUXT_PUBLIC_DISCORD_CLIENT_ID}.discordsays.com/_icons`,
+            url: `/_icons`,
             publicApiFallback: false,
         },
     },
